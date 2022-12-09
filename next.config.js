86a17fdfbd7d0e5ec7aa5dc1a91fbce391666c71
songseul <song.codestates@gmail.com>
2022-12-09 17:27:07 +0900

    api 요청 주소 배포주소로 변경

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const { redirect } = require('next/dist/server/api-utils');
 
 /** @type {import('next').NextConfig} */
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 const nextConfig = {
   reactStrictMode: true,
